9ec7811d5cd4aa4623301044af90741ed60c1a76
Sondos Ahmed <sondosalshami@gmail.com>
2020-08-19 09:47:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 function Scooter(year, color, model) {
-    return {
-    year, // don't forget ES6 power-tools, this is the same as `name: name`
-    color,
-    model,
-  }
+    this.year = year;
+    this.color=color;
+    this.model=model;
 }
